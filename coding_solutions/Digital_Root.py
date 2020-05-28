1dd7fbeaaae970f55ca49405d0645909f6fc28b9
DJasmineJoyline <42811221+DJasmineJoyline@users.noreply.github.com>
2020-05-28 18:12:18 +0530

    Update Digital_Root.py

--- coding_solutions/Digital_Root.py
+++ coding_solutions/Digital_Root.py
@@ -9,9 +9,9 @@
 
 
 def digital_root(num):
-    sum=0
-    for i in num:
-        sum=sum+int(i)
-    return sum
-num=input('Enter the num ')
+    if(num<10):
+        return num
+    else:
+        return digital_root((num%10)+digital_root(num//10))1
+num=int(input('Enter the num '))
 print(digital_root(num))
