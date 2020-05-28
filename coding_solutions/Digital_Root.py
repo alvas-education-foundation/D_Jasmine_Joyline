8d3f4369c7d705e39f61f66ca2de00b5a9c01975
DJasmineJoyline <42811221+DJasmineJoyline@users.noreply.github.com>
2020-05-28 17:57:20 +0530

    Update Digital_Root.py

--- coding_solutions/Digital_Root.py
+++ coding_solutions/Digital_Root.py
@@ -1,3 +1,13 @@
+#Description:
+#A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+#digit_root(0)= 0
+
+#digital_root(16)
+#=> 1 + 6
+#=> 7
+
+
+
 def digital_root(num):
     sum=0
     for i in num:
