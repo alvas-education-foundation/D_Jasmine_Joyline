04878a6ee98efb1462cc83cc9a5f809ba4fe4dfd
DJasmineJoyline <42811221+DJasmineJoyline@users.noreply.github.com>
2020-05-29 17:57:16 +0530

    Create Armstrong.java

---
+++ coding_solutions/Armstrong.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        int originalNumber, remainder, result = 0, n = 0;
+
+        originalNumber = number;
+
+        for (;originalNumber != 0; originalNumber /= 10, ++n);
+
+        originalNumber = number;
+
+        for (;originalNumber != 0; originalNumber /= 10)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+        }
+
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+}
