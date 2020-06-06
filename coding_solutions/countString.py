632da54c20f22bc01498534ac805c0b16f73ea9f
DJasmineJoyline <42811221+DJasmineJoyline@users.noreply.github.com>
2020-06-06 15:13:27 +0530

    Update countString.py

--- coding_solutions/countString.py
+++ coding_solutions/countString.py
@@ -1,3 +1,10 @@
+#write a Python program to count the number of strings, provided string length is 2 or more and the first and last character are same from a given list of strings.
+#Eg:
+#Input:
+#list1['hia', 'aba' , '363']
+#Output:
+#Number of strings with first and last cahracter is same: 2
+
 l=[]
 n=int(input('Enter value of n: '))
 for i in range(n):
@@ -7,4 +14,4 @@ count=0
 for i in l:
     if(i[0]==i[-1]):
         count+=1
-print(count)
+print('Number of strings with first and last cahracter is same:',count)
